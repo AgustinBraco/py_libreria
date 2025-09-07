c09f596e0bc530a9863d33416808870a3542d976
agustin <bracoagustin@gmail.com>
2025-09-06 21:00:04 -0300

    [Main] - Initial commit

---
+++ apps/libros/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+
+class Libro(models.Model):
+    titulo = models.CharField(max_length=100)
+    descripcion = models.TextField()
+    fecha_publicacion = models.DateField()
+    paginas = models.IntegerField()
+    disponible = models.BooleanField(default=True)
+
+    def __str__(self):
+        return self.titulo
